6e12fa04dfc651af43ffc29f613b60195fbfb91d
rkumarp <issac7@gmail.com>
2019-12-03 00:47:57 -0600

    not there yet

--- sketch.js
+++ sketch.js
@@ -1,15 +1,12 @@
 var label = "";
 var target = "7284";
+var rndm=""
 
-var potential = [];
-var population = 100;
-var populationx = population;
+var population = [];
+var populationSize = 1000;
+var populationBest=[]
 
-var stage1 = false;
 
-function setup() {
-  createCanvas(windowWidth - 50, windowHeight - 20);
-}
 
 const generateLabel = x => {
   o = [];
@@ -19,80 +16,146 @@ const generateLabel = x => {
   return o.join("");
 };
 
+const calculateFitness=(x)=>{
+  console.log(x.length)
+  ff=x.map(e=>{return {guess:e,distance:fitness(e,target)}})
+  if (ff.includes(target)){
+    noLoop()
+  }
+  return ff.filter(e=>e.distance<.9)
+
+  // console.log(populationBest)
+  
+}
+
+
+
 const fitness = (v, t) => {
   var score = 0;
   for (let idx in v) {
     score += v[idx] === t[idx] ? 1 : 0;
   }
-  if (score) {
-    potential.push({ guess: v, score, prob: parseFloat(score / t.length) });
-  }
-  return score;
+  return 1/(score+1);
 };
 
-const stopLoop = () => (stage1 = false);
 
-const createPopulation = () => {
-  if (!stage1) {
-    rndm = generateLabel(4);
-    score = fitness(rndm, target);
+const createPopulation = (ct) => {
+  for (let i=0; i < ct; i++){
+    rndm=generateLabel(4)
+    population.push(rndm);
+    // score = fitness(rndm, target);
 
-    // score = fitness("rndm", "rndm");
-    text(target, width / 2, (height - 50) / 2);
-    text(rndm, width / 2, height / 2);
-    text(score, width / 2, (height + 50) / 2);
-    console.log(rndm, target, score, stage1, populationx);
-    populationx--;
-    stage1 = populationx <= 0 && !stage1;
+    // // score = fitness("rndm", "rndm");
+    // text(target, width / 2, (height - 50) / 2);
+    // text(score, width / 2, (height + 50) / 2);
+    // populationx--;
+    // stage1 = populationx <= 0 && !stage1;
+  }
+    
     // console.log("Stopped");
+  };
+
+// const generateFrom = (v, x, y) => {
+//   console.log(x.guess, y.guess);
+//   const x_ = x.guess.slice(0, 2);
+//   const y_ = y.guess.slice(2);
+
+//   potential.splice(potential.indexOf(x), 1);
+//   potential.splice(potential.indexOf(y), 1);
+
+//   ///mutate
+//   n = x_ + y_;
+//   score = fitness(n, target);
+//   if (score > 2) {
+//     potential.push({ guess: n, gen: v, score });
+//   }
+
+//   console.log(potential);
+// };
+
+const nextGeneration = (x) => {
+  o=[]
+  for (let c=0;c<x.length;c++){
+    // pickFromBest(x)
+    idx1=floor(random(x.length))
+    idx2=floor(random(x.length))
+
+    chosen1=x[idx1]
+    // chosen2=x[idx2]
+    console.log("removed",x.splice(idx1,1))
+    // x.splice(idx2,1)
+    // console.log("removed",x.splice(idx2,1))
+
+
+    // console.log(chosen1,chosen2)
+
+
+    // mutate()
+    mutatedChoosen1=shuffle([...chosen1.guess]).join("")
+    // mutatedChoosen2=shuffle([...chosen2.guess]).join("")
+    // console.log(mutatedChoosen1,mutatedChoosen2)
+
+
+    // crossover()
+    // add back to populatioBest
+    // chosen.guess=mutatedChoosen
+    // console.log(chosen)
+
+    // mutatect1=floor(random(3))
+    // mutatect2=4-mutatect1
+    // p1=mutatedChoosen1.slice(mutatect1)
+    // p2=mutatedChoosen2.slice(mutatect2)
+    // console.log(p1,p2)
+
+    // console.log("Added",o.push([p1,p2].join("")))
+    console.log("Added",o.push(mutatedChoosen1))
+
   }
+  console.log("o",o.length)
+  return o
+  
+  // remove original from populationBest
+  // make populationBest population
+  
+  
 };
 
-const generateFrom = (v, x, y) => {
-  console.log(x.guess, y.guess);
-  const x_ = x.guess.slice(0, 2);
-  const y_ = y.guess.slice(2);
+function setup() {
+  createCanvas(windowWidth - 50, windowHeight - 20);
+  createPopulation(populationSize)
+}
 
-  potential.splice(potential.indexOf(x), 1);
-  potential.splice(potential.indexOf(y), 1);
+const normalize=(x)=>{
+  s=x.reduce((o,e)=>o+e.distance,0)
+  // console.log(x.map(e=>{return {prob:1-e.distance/s,...e}}))
+  
+}
 
-  ///mutate
-  n = x_ + y_;
-  score = fitness(n, target);
-  if (score > 2) {
-    potential.push({ guess: n, gen: v, score });
-  }
 
-  console.log(potential);
-};
+const pickFromBest=(x)=>{
+  // find the high prob in the list and get one 
+  x[floor(random(x.length-1))]
 
-const nextGeneration = () => {
-  if (stage1) {
-    console.log(potential.length);
-    potential.map(e => console.log(e));
-
-    // now we need to create another population by breeding between potentials and generate random
-
-    gap = population - potential.length;
-    console.log(gap);
-    //breeding
-
-    for (let v = 0; v <= 30; v++) {
-      generateFrom(
-        v,
-        potential[floor(random(potential.length))],
-        potential[floor(random(potential.length))]
-      );
-    }
-    //mutate
-
-    noLoop();
-  }
-};
+}
 
 function draw() {
+
   background(187);
   textSize(20);
-  createPopulation();
-  nextGeneration();
+  text(target, width / 2, (height - 50) / 2);
+  text(rndm, width / 2, height / 2);
+
+
+  populationBest=calculateFitness(population)
+  // normalize(populationBest)
+  o=nextGeneration(populationBest)
+  console.log("o is",o)
+  if (o.length===0){
+    createPopulation(populationSize)
+  } 
+  else{
+    population=o
+
+  }
 }
+
